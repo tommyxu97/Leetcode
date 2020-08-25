29201df4a90d97b8f220d88a234abb09556e75b0
TommyXu97 <377935382@qq.com>
2020-08-25 13:48:05 +0800

    Update

---
+++ src/Offer45.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class Offer45 {
+
+    public static void main(String[] args) {
+        Offer45 offer45 = new Offer45();
+        System.out.println(offer45.minNumber(new int[]{3,30,34,9,5}));
+    }
+
+    public String minNumber(int[] nums) {
+        String[] strings = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strings[i] = String.valueOf(nums[i]);
+        Arrays.sort(strings, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder res = new StringBuilder();
+        for(String s : strings)
+            res.append(s);
+        return res.toString();
+    }
+}
