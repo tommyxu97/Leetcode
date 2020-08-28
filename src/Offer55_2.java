216044b7bd7defc9f3c9dec857e314d36853efc4
TommyXu97 <377935382@qq.com>
2020-08-28 18:09:53 +0800

    Update

---
+++ src/Offer55_2.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Offer55_2 {
+
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if (left == -1) return -1;
+        int right = recur(root.right);
+        if (right == -1) return -1;
+        return Math.abs(left - right) < 2? Math.max(left, right) + 1: -1;
+    }
+}
