26698cdd194f4aedd4b054ee8566b289c70ac745
TommyXu97 <377935382@qq.com>
2020-08-30 11:03:14 +0800

    Update

---
+++ src/Offer57.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+public class Offer57 {
+
+    // 双指针
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum < target) i++;
+            else if (sum > target) j--;
+            else return new int[]{nums[i], nums[j]};
+        }
+        return new int[]{};
+    }
+
+    // HashSet
+    public int[] twoSum2(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.contains(target - num)) return new int[]{num, target - num};
+            else set.add(num);
+        }
+        return new int[]{};
+    }
+}
