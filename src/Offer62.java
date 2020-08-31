467121e4142f83c6ba052c3d96e5be0b2f58dceb
TommyXu97 <377935382@qq.com>
2020-08-31 16:25:20 +0800

    Update

---
+++ src/Offer62.java
@@ -0,0 +1,11 @@
+public class Offer62 {
+
+    // 约瑟夫环问题
+    public int lastRemaining(int n, int m) {
+        int pos = 0;
+        for (int i = 2; i <= n; i++) {
+            pos = (pos + m) % i;
+        }
+        return pos;
+    }
+}
