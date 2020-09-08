2c068eb1643acab8766e74ca1be52ed5e5b3bf1e
TommyXu97 <377935382@qq.com>
2020-09-09 00:16:06 +0800

    Update

---
+++ src/ByteDance/String/Q2.java
@@ -0,0 +1,21 @@
+package ByteDance.String;
+
+public class Q2 {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        int minLen = Integer.MAX_VALUE;
+        for (String str: strs) {
+            minLen = Math.min(str.length(), minLen);
+        }
+        label: for (int i = 0; i < minLen; i++) {
+            char temp = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != temp) break label;
+            }
+            stringBuilder.append(temp);
+        }
+        return stringBuilder.toString();
+    }
+}
