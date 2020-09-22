d731d18b9f4b10a586542e46c66997a802a5cd70
TommyXu97 <377935382@qq.com>
2020-09-22 19:22:30 +0800

    Update

---
+++ src/Algorithm/Q78.java
@@ -0,0 +1,21 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q78 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> temp = new ArrayList<>(res.get(j));
+                temp.add(nums[i]);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+}
