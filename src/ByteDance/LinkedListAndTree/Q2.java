9dfa72182684df960ecd1bdcc90e8d8ddbe2e37a
TommyXu97 <377935382@qq.com>
2020-10-07 14:44:47 +0800

    Update

---
+++ src/ByteDance/LinkedListAndTree/Q2.java
@@ -0,0 +1,23 @@
+package ByteDance.LinkedListAndTree;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Q2 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = null, cur = head, next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
