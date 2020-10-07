8d5e081a75c94e706d2eb9cedc3ff47a8d377066
TommyXu97 <377935382@qq.com>
2020-10-07 14:40:48 +0800

    Update

---
+++ src/ByteDance/LinkedListAndTree/Q1.java
@@ -0,0 +1,48 @@
+package ByteDance.LinkedListAndTree;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Q1 {
+    // 循环
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node1 = l1, node2 = l2, cur = dummy;
+        while(node1 != null && node2 != null) {
+            if (node1.val <= node2.val) {
+                cur.next = node1;
+                node1 = node1.next;
+            } else {
+                cur.next = node2;
+                node2 = node2.next;
+            }
+            cur = cur.next;
+        }
+        if (node1 != null) cur.next = node1;
+        if (node2 != null) cur.next = node2;
+        return dummy.next;
+    }
+
+    // 递归
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return l1 == null? l2: l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
