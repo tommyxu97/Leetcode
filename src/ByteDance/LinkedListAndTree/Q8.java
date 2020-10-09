d27c27666963d4f9decd51307c13f8bdbf5046be
TommyXu97 <377935382@qq.com>
2020-10-09 14:17:31 +0800

    Update

---
+++ src/ByteDance/LinkedListAndTree/Q8.java
@@ -0,0 +1,30 @@
+package ByteDance.LinkedListAndTree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Q8 {
+    // 与主题库Q236，剑指Offer68-2相同
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
