36cbe1e3ed17c8f36b6b46b7a3b1f3ffb6f11fad
TommyXu97 <377935382@qq.com>
2020-10-09 11:50:15 +0800

    Update

---
+++ src/Algorithm/Q141.java
@@ -0,0 +1,25 @@
+package Algorithm;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Q141 {
+    // 快慢指针
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
