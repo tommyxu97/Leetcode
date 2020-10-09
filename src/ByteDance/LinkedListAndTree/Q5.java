01a0d307257ae39f215817826449abf15c12df33
TommyXu97 <377935382@qq.com>
2020-10-09 14:17:04 +0800

    Update

---
+++ src/ByteDance/LinkedListAndTree/Q5.java
@@ -0,0 +1,20 @@
+package ByteDance.LinkedListAndTree;
+
+public class Q5 {
+    // 与主题库Q142相同
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
