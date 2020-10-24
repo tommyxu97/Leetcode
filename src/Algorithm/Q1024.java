5e16d5b73a783ba997488fe679822d0aaac8b9f3
TommyXu97 <377935382@qq.com>
2020-10-24 19:58:28 +0800

    Update

--- src/Algorithm/Q1024.java
+++ src/Algorithm/Q1024.java
@@ -3,6 +3,7 @@ package Algorithm;
 import java.util.Arrays;
 
 public class Q1024 {
+    // 动态规划,2ms
     public int videoStitching(int[][] clips, int T) {
         int[] dp = new int[T + 1];
         Arrays.fill(dp, Integer.MAX_VALUE / 2);
@@ -15,5 +16,25 @@ public class Q1024 {
             }
         }
         return dp[T] == Integer.MAX_VALUE / 2? -1: dp[T];
+    }
+
+    // 贪心,0ms
+    public int videoStitching2(int[][] clips, int T) {
+        int[] maxRight = new int[T];
+        int res = 0, last = 0, pre = 0;
+        for (int[] clip: clips) {
+            if (clip[0] < T) {
+                maxRight[clip[0]] = Math.max(maxRight[clip[0]], clip[1]);
+            }
+        }
+        for (int i = 0; i < T; i++) {
+            last = Math.max(last, maxRight[i]);
+            if (i == last) return -1;
+            if (i == pre) {
+                res++;
+                pre = last;
+            }
+        }
+        return res;
     }
 }
