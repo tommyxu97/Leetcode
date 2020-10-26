ce41d377f4081d6bcacf24d562aaaae2932428fe
TommyXu97 <377935382@qq.com>
2020-10-26 13:20:26 +0800

    Update

--- src/Algorithm/Q845.java
+++ src/Algorithm/Q845.java
@@ -1,6 +1,7 @@
 package Algorithm;
 
 public class Q845 {
+    // 3ms
     public int longestMountain(int[] A) {
         if (A == null || A.length < 3) return 0;
         int[] left = new int[A.length];
@@ -16,6 +17,26 @@ public class Q845 {
             if (left[i] != 0 && right[i] != 0) {
                 res = Math.max(res, left[i] + right[i] + 1);
             }
+        }
+        return res;
+    }
+
+    // 2ms
+    public int longestMountain2(int[] A) {
+        if (A == null || A.length < 3) return 0;
+        int res = 0, index = 1;
+        while (index < A.length) {
+            int increase = 0, decrease = 0;
+            while (index < A.length && A[index] > A[index - 1]) {
+                increase++; index++;
+            }
+            while (index < A.length && A[index] < A[index - 1]) {
+                decrease++; index++;
+            }
+            if (increase > 0 && decrease > 0) {
+                res = Math.max(res, increase + decrease + 1);
+            }
+            while (index < A.length && A[index] == A[index -1]) index++;
         }
         return res;
     }
