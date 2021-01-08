48594281cc2948ecf5fa970f47dd9ad460b89b09
TommyXu97 <i@xht97.cn>
2021-01-08 16:04:30 +0800

    Update

--- src/Algorithm/Q189.java
+++ src/Algorithm/Q189.java
@@ -10,4 +10,21 @@ public class Q189 {
         System.arraycopy(nums,  0, newNums, k, nums.length - k);
         System.arraycopy(newNums, 0, nums, 0, newNums.length);
     }
+
+    public void rotate2(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return;
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++; end--;
+        }
+    }
 }
