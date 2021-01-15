ece02fb9249895c08e721efe2f372e6bc6be7740
TommyXu97 <i@xht97.cn>
2021-01-15 21:42:47 +0800

    Update

--- src/Algorithm/Q947.java
+++ src/Algorithm/Q947.java
@@ -1,9 +1,13 @@
 package Algorithm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Q947 {
     private int[] parent;
     private int count;
 
+    // 30ms
     public int removeStones(int[][] stones) {
         if (stones == null || stones.length < 2) return 0;
         parent = new int[stones.length];
@@ -16,6 +20,21 @@ public class Q947 {
             }
         }
         return stones.length - count;
+    }
+
+    // 5ms
+    public int removeStones2(int[][] stones) {
+        if (stones == null || stones.length < 2) return 0;
+        parent = new int[20000];
+        for (int i = 0; i < parent.length; i++) parent[i] = i;
+        for (int[] stone: stones) {
+            union(stone[0], stone[1] + 10000);
+        }
+        Set<Integer> seen = new HashSet<>();
+        for (int[] stone: stones) {
+            seen.add(find(stone[0]));
+        }
+        return stones.length - seen.size();
     }
 
     private int find(int x) {
