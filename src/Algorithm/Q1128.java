d69cd850398ac5b0440bb921d1b34a0565cd97a1
TommyXu97 <377935382@qq.com>
2021-01-26 22:30:17 +0800

    Update

---
+++ src/Algorithm/Q1128.java
@@ -0,0 +1,29 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Q1128 {
+    // 13ms
+    public int numEquivDominoPairs(int[][] dominoes) {
+        if (dominoes == null || dominoes.length == 0) return 0;
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int[] domino: dominoes) {
+            if (domino[0] > domino[1]) {
+                swap(domino);
+            }
+            int hashcode = Arrays.hashCode(domino);
+            int num = map.getOrDefault(hashcode, 0);
+            res += num;
+            map.put(hashcode, num + 1);
+        }
+        return res;
+    }
+
+    private void swap(int[] domino) {
+        int temp = domino[0];
+        domino[0] = domino[1];
+        domino[1] = temp;
+    }
+}
