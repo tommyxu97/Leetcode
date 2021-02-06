c87f45efcdde066e7121dbd63a5fbcad39c1082f
tommyxu97 <377935382@qq.com>
2021-02-07 00:07:02 +0800

    Update

---
+++ src/Algorithm/Q64.java
@@ -0,0 +1,17 @@
+package Algorithm;
+
+public class Q64 {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.length; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int i = 1; i < grid[0].length; i++) dp[0][i] = dp[0][i - 1] + grid[0][i];
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
