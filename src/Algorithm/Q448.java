b7e9592f64b0339a0ecc721feb4781fd0e5ac0c2
TommyXu97 <377935382@qq.com>
2021-02-13 11:31:28 +0800

    Update

--- src/Algorithm/Q448.java
+++ src/Algorithm/Q448.java
@@ -1,11 +1,9 @@
 package Algorithm;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Q448 {
+    // 20ms
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
         Set<Integer> set = new HashSet<>();
@@ -13,6 +11,18 @@ public class Q448 {
         List<Integer> res = new ArrayList<>(n - set.size());
         for (int i = 1; i <= n; i++) {
             if (!set.contains(i)) res.add(i);
+        }
+        return res;
+    }
+
+    // 4ms
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
+        int n = nums.length;
+        boolean[] flags = new boolean[n + 1];
+        for (int num: nums) flags[num] = true;
+        List<Integer> res = new LinkedList<>();
+        for (int i = 1; i < flags.length; i++) {
+            if (!flags[i]) res.add(i);
         }
         return res;
     }
