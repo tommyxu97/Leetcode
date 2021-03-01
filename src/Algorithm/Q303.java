f86fd664925d8ef7372a35bbf02ef0f6c0e648df
TommyXu97 <i@xht97.cn>
2021-03-01 16:21:56 +0800

    Update

---
+++ src/Algorithm/Q303.java
@@ -0,0 +1,28 @@
+package Algorithm;
+
+public class Q303 {
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+class NumArray {
+    private int[] sums;
+
+    public NumArray(int[] nums) {
+        if (nums.length == 0) return;
+        sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (sums == null) return 0;
+        return i == 0? sums[j]: sums[j] - sums[i - 1];
+    }
+}
