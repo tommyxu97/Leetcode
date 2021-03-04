ae3e7091c617d4d363026d9d77a07aa55790daee
TommyXu97 <i@xht97.cn>
2021-03-04 22:42:33 +0800

    Update

---
+++ src/Algorithm/Q354.java
@@ -0,0 +1,33 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Q354 {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) return 0;
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o2[1] - o1[1];
+                }
+            }
+        });
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            res = Math.max(res, f[i]);
+        }
+        return res;
+    }
+}
