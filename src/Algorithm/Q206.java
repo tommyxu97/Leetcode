dd5f2c348a4c85bfe0f7c2e9e9204de5e37ee152
TommyXu97 <377935382@qq.com>
2021-03-18 22:33:58 +0800

    Update

---
+++ src/Algorithm/Q206.java
@@ -0,0 +1,25 @@
+package Algorithm;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Q206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null, next = null, cur = head;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
