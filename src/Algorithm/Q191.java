fcb3c61bbf25dbf4e74f8210d9107b6dd969824c
TommyXu97 <377935382@qq.com>
2021-03-22 23:02:32 +0800

    Update

---
+++ src/Algorithm/Q191.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+public class Q191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n &= n - 1;
+            res++;
+        }
+        return res;
+    }
+}
