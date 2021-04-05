910a7af7a74d09c9310ccef29555c83028d3fb02
TommyXu97 <377935382@qq.com>
2021-04-05 23:28:51 +0800

    Update

---
+++ src/Algorithm/Q88.java
@@ -0,0 +1,10 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+public class Q88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+}
