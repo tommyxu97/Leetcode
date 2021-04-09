1e7f34dbf92deb2ae44dedd846561c3e2f95d6dc
TommyXu97 <377935382@qq.com>
2021-04-09 19:23:18 +0800

    Update

---
+++ src/Algorithm/Q154.java
@@ -0,0 +1,18 @@
+package Algorithm;
+
+public class Q154 {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}
